218b6b57fac435e1fa07f6890af7d6a327617630
sucihasanahbertha <48718570+sucihasanahbertha@users.noreply.github.com>
2019-03-19 19:11:31 +0700

    Create findword.cpp

---
+++ findword.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main ()
+{
+  int
+  char puzzle[15][15]= { {'t', 'g', 'b', 'w', 'w', 'i', 'n', 't', 'e', 'r', 'w', 's', 'e', 's', 'n'},
+                         {'a', 'a', 'u', 'n', 't', 't', 'm', 'm', 'h', 'f', 'o', 'o', 'd', 'n', 'b'},
